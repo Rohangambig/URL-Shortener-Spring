ad3d31142374073651c765fd162281910767105a
Rohangambig <rohanambig599@gmail.com>
2026-08-16 12:54:47 +0530

    just a project setup - checking health

---
+++ src/main/java/url_shortnert/url_shortner/controller/Controller.java
@@ -0,0 +1,14 @@
+package url_shortnert.url_shortner.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("health")
+    public String health() {
+        return "OK";
+    }
+}
